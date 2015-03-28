835e93777ed981a58ce8ab1f687f3b420438ea1e
LT <ltrain777@hotmail.com>
2015-03-28 12:58:12 -0700

    make it specific to ltrain777hotmail

--- TodoSPA/App/Scripts/app.js
+++ TodoSPA/App/Scripts/app.js
@@ -21,17 +21,16 @@ angular.module('todoApp', ['ngRoute','AdalAngular'])
     var endpoints = {
 
         // Map the location of a request to an API to a the identifier of the associated resource
-        "Enter the root location of your To Go API here, e.g. https://contosotogo.azurewebsites.net/":
-            "Enter the App ID URI of your To Go API here, e.g. https://contoso.onmicrosoft.com/ToGoAPI",
+        "https://localhost:44327": "https://ltrain777hotmail.onmicrosoft.com/ToGoAPI"
     };
 
     adalProvider.init(
         {
-            tenant: 'Enter your tenant name here e.g. contoso.onmicrosoft.com',
-            clientId: 'Enter your client ID here e.g. e9a5a8b6-8af7-4719-9821-0deef255f68e',
+            tenant: 'ltrain777hotmail.onmicrosoft.com',
+            clientId: 'a08d5601-ca89-409e-b630-921f1698bf5b',
             extraQueryParameter: 'nux=1',
             endpoints: endpoints,
-            //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.  
+            cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.  
             // Also, token acquisition for the To Go API will fail in IE when running on localhost, due to IE security restrictions.
         },
         $httpProvider
